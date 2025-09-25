819bc7fb04137735f40fce13a4667a0147a2f9ab
Nan Ding <Nan.Ding@amd.com>
2025-09-25 16:16:28 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-hipcc -O3 overlap_rccl_hipblas_all2all.cpp -o overlap_rccl_hipblas_all2all \
-  -lrccl -lhipblas -lmpi
-
-hipcc -O3 overlap_rccl_hipblas_allreduce.cpp -o overlap_rccl_hipblas_allreduce \
-  -lrccl -lhipblas -lmpi
